c6c7fee9b6fb83ecf20540498325f18d53b3522f
haneulab <hanchoi@ucdavis.edu>
2022-06-22 01:09:21 -0700

    new facebook page src url requested by client

--- src/components/index/FacebookFeed.tsx
+++ src/components/index/FacebookFeed.tsx
@@ -6,7 +6,7 @@ const FacebookFeed: React.FC = () => {
       </h3>
       <iframe
         className="w-full overflow-hidden h-96"
-        src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FYakitoriYuchan%2F&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=912214705987872"
+        src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FYuchanShokudo&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=912214705987872"
         style={{ overflow: 'hidden' }}
         width={'100%'}
         height={'100%'}
